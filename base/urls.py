6337d9017867925c9a8a39c7944f94dc0aab5bc6
nismal <nismalm14@gmail.com>
2023-11-08 12:51:47 +0530

    check_username_availability

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 
-from base.views import home, login_page, register_page, add_cart, cart, remove_cart_items, orders, success, logout_view
+from base.views import home, login_page, register_page, add_cart, cart, remove_cart_items, orders, success, logout_view, \
+    check_username_availability
+
 urlpatterns = [
     path('',home,name='home'),
     path('login/',login_page, name='login'),
@@ -13,5 +15,6 @@ urlpatterns = [
 
     path('add_cart/<int:id>', add_cart, name='add_cart'),
     path('remove_cart_items/<int:pk>', remove_cart_items, name='remove_cart_items'),
+    path('check-check_username_availability/', check_username_availability, name='check_username_availability'),
 
 ]
